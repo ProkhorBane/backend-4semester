5ac45e7e4cd05db91aa37e5d20a316dfe8f979e2
ProkhorBane <prokhor.topin@mail.ru>
2026-02-11 15:17:26 +0300

    Добавлены обработчики

--- my-aspnet-project/my-aspnet-project/obj/Debug/net9.0/my-aspnet-project.AssemblyInfo.cs
+++ my-aspnet-project/my-aspnet-project/obj/Debug/net9.0/my-aspnet-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-aspnet-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1396154d23cbdd9e5cb6872909162af61acaeed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-aspnet-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-aspnet-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
