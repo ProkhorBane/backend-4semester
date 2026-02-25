e1ef186545c55f700e75500c7d704c82b3de210c
ProkhorBane <prokhor.topin@mail.ru>
2026-02-25 14:24:43 +0300

    Добавлены обработчики с передачей через тело запроса и query-параметры

--- my-aspnet-project/my-aspnet-project/Program.cs
+++ my-aspnet-project/my-aspnet-project/Program.cs
@@ -1,5 +1,88 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+
+List<Person> users = new List<Person>
+{
+    new() { Id = Guid.NewGuid().ToString(), Name = "Stas", Age = 27 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Prokhor", Age = 19 },
+    new() { Id = Guid.NewGuid().ToString(), Name = "Gleb", Age = 22 }
+};
+
+app.MapGet("api/users", () => users);
+
+app.MapGet("api/users/search", (string? name, int? minAge) =>
+{
+    var filtered = users.AsEnumerable();
+    if  (!string.IsNullOrEmpty(name)) 
+        filtered = filtered.Where(u => u.Name.Contains(name));
+    if  (minAge.HasValue)
+        filtered = filtered.Where(u => u.Age >= minAge.Value);
+    
+    return Results.Ok(filtered);
+});
+
+app.MapGet("api/users/{id}", (string id) =>
+{
+    Person? user = users.FirstOrDefault(u => u.Id == id);
+    if (user is null) return Results.NotFound(new {message = "User not found"});
+    
+    return Results.Ok(user);
+});
+
+app.MapPost("api/users/create", (Person user) =>
+{
+    user.Id = Guid.NewGuid().ToString();
+
+    users.Add(user);
+    return Results.Created($"api/users/{user.Id}", user);
+});
+
+app.MapPost("api/users/search", (string? name, int? minAge) =>
+{
+    var filtered = users.AsEnumerable();
+    if  (!string.IsNullOrEmpty(name)) 
+        filtered = filtered.Where(u => u.Name.Contains(name));
+    if  (minAge.HasValue)
+        filtered = filtered.Where(u => u.Age >= minAge.Value);
+    
+    return Results.Ok(filtered);
+});
+
+app.MapPut("api/users/{id}", (string id, Person updatedUser) =>
+{
+    var existing = users.FirstOrDefault(u => u.Id == id);
+    if (existing is null) return Results.NotFound(new { message = "User not found" });
+
+    existing.Name = updatedUser.Name;
+    existing.Age = updatedUser.Age;
+
+    return Results.Ok(existing);
+});
+
+app.MapPatch("api/users/{id}/update", (string id, JsonElement patchData) =>
+{
+    var user = users.FirstOrDefault(u => u.Id == id);
+    if (user is null) return Results.NotFound(new { message = "User not found" });
+
+    foreach (var patch in patchData.EnumerateObject())
+    {
+        switch (patch.Name.ToLower())
+        {
+            case "name":
+                user.Name = patch.Value.GetString() ?? "";
+                break;
+            case "age":
+                if (patch.Value.TryGetInt32(out int age))
+                    user.Age = age;
+                break;
+        }
+    }
+    
+    return Results.Ok(user);
+});
 
 app.MapGet("/", () => "Hello ");
 app.MapGet("/about", () => "мой первый aspnet проект");
@@ -7,3 +90,10 @@ app.MapGet("/user",  () => new { id = 1, Name = "Prokhor" });
 
 
 app.Run();
+
+public class Person
+{
+    public string Id { get; set; } = "";
+    public string Name { get; set; } = "";
+    public int Age { get; set; }
+}
--- my-aspnet-project/my-aspnet-project/obj/Debug/net9.0/my-aspnet-project.AssemblyInfo.cs
+++ my-aspnet-project/my-aspnet-project/obj/Debug/net9.0/my-aspnet-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-aspnet-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1396154d23cbdd9e5cb6872909162af61acaeed6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afae0d7f641bbbf3f4c0e34ced4818f876e3d5bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-aspnet-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-aspnet-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
